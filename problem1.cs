7b1f360a0ab4a50dea7353e14d9d0965bc89c1c3
MohamedHalima7 <95139069+MohamedHalima7@users.noreply.github.com>
2021-11-27 18:00:47 +0200

    Add files via upload

---
+++ problem1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Console1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> HalimaList = new LinkedList<string>();
+
+            LinkedListNode<string> HalimaNode = HalimaList.AddFirst("H1");
+            HalimaList.AddFirst("H2");
+            HalimaList.AddFirst("H3");
+            HalimaList.AddBefore(HalimaNode, "H4");
+            HalimaList.AddLast("Far away");
+
+            Console.WriteLine("before being removed:  ");
+
+            foreach (var item in HalimaList)
+            {
+                Console.Write(item + ",");
+            }
+
+            HalimaList.RemoveFirst();
+            HalimaList.RemoveLast();
+            HalimaList.Remove("H4");
+            HalimaList.Remove("H1");
+
+            Console.WriteLine();
+            Console.WriteLine("after being removed: ");
+
+            foreach (var item in HalimaList)
+            {
+                Console.Write(item + ",");
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
